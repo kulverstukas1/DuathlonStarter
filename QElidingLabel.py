df3aa677aca13a40be8fcfa2f89fcd0d2dfd36c0
Kulverstukas <kulverstukas.k@gmail.com>
2017-07-13 11:41:17 +0300

    sukurta sub-klase kad prideti metoda kuris automatiskai uzdeda daugtaski kai eilute netelpa

---
+++ QElidingLabel.py
@@ -0,0 +1,22 @@
+from PyQt5 import QtCore, QtWidgets
+from PyQt5.QtGui import QFontMetrics
+
+'''
+    This is a subclass of QLabel to add an additional method
+    which elides (adds ... to the right of a string if it's
+    too long to fit in a single line).
+    This is meant to be added within the Qt Designer, by
+    promoting your label with these settings:
+        Base class name: QLabel
+        Promoted class name: QElidingLabel
+        Header file: QElidingLabel
+        Global include: unchecked
+    This way QElidingLabel is always set when UI is compiled.
+'''
+
+class QElidingLabel(QtWidgets.QLabel):
+    def setElidedText(self, text):
+        metrics = QFontMetrics(self.font())
+        elided = metrics.elidedText(text, QtCore.Qt.ElideRight, self.width())
+        self.setText(elided)
+        
